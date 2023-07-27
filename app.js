523d71f244d176a984dadfd0a998f245905183d1
msTheBear <michalthebear@gmail.com>
2023-07-27 11:51:42 -0700

    Dev Portfolio

---
+++ app.js
@@ -0,0 +1,317 @@
+function projectsTab() {
+    event.preventDefault();
+    $('[href="#projects"]').tab('show');
+}
+
+function home() {
+    event.preventDefault();
+    $('[href="#aboutMe"]').tab('show');
+}
+
+
+
+function projectSwitch1() {
+    document.getElementById("p_main").src = "./projects/boopgames/main.webp";
+    document.getElementById("p_name").innerHTML = "BOOPGAMES";
+    document.getElementById("p_github").href = "https://github.com/msTheBear/BoopGames";
+    document.getElementById("p_link").innerHTML = "WEBSITE";
+    document.getElementById("p_link").href = "https://msthebear.github.io/BoopGames/";
+
+    document.getElementById("p_description").innerHTML = "Game database website built using RAWG's API. Users can find games, view screenshots, check ratings, read game description and game info (platform availability, release date, genre, developer).";
+
+    document.getElementById("mainLink").href = "https://msthebear.github.io/BoopGames/";
+
+    document.getElementById("p_mini1").src = "./projects/boopgames/boopgames_th.webp";
+    document.getElementById("p_mini2").src = "./projects/boopgames/screenshot_1.webp";
+    document.getElementById("p_mini3").src = "./projects/boopgames/screenshot_2.webp";
+    document.getElementById("p_mini4").src = "./projects/boopgames/screenshot_3.webp";
+
+    document.getElementById("p_mini_modal1").src = "./projects/boopgames/boopgames_th.webp";
+    document.getElementById("p_mini_modal2").src = "./projects/boopgames/screenshot_1.webp";
+    document.getElementById("p_mini_modal3").src = "./projects/boopgames/screenshot_2.webp";
+    document.getElementById("p_mini_modal4").src = "./projects/boopgames/screenshot_3.webp";
+}
+
+function projectSwitch2() {
+    document.getElementById("p_main").src = "./projects/msthebear/main.webp";
+    document.getElementById("p_name").innerHTML = "MICHAL SZYMCZAK PORTFOLIO";
+    document.getElementById("p_github").href = "https://github.com/msTheBear/Portfolio-Website_a";
+    document.getElementById("p_link").innerHTML = "WEBSITE";
+    document.getElementById("p_link").href = "https://msthebear.github.io/Portfolio-Website_a/";
+
+    document.getElementById("p_description").innerHTML = "Videography portfolio with showreel built into the TV, showcasing Videographer's projects, equipment and software he uses and clients he works with.";
+
+    document.getElementById("mainLink").href = "https://msthebear.github.io/Portfolio-Website_a/";
+
+    document.getElementById("p_mini1").src = "./projects/msthebear/msthebear_th.webp";
+    document.getElementById("p_mini2").src = "./projects/msthebear/screenshot_1.webp";
+    document.getElementById("p_mini3").src = "./projects/msthebear/screenshot_2.webp";
+    document.getElementById("p_mini4").src = "./projects/msthebear/screenshot_3.webp";
+
+    document.getElementById("p_mini_modal1").src = "./projects/msthebear/msthebear_th.webp";
+    document.getElementById("p_mini_modal2").src = "./projects/msthebear/screenshot_1.webp";
+    document.getElementById("p_mini_modal3").src = "./projects/msthebear/screenshot_2.webp";
+    document.getElementById("p_mini_modal4").src = "./projects/msthebear/screenshot_3.webp";
+}
+
+function projectSwitch3() {
+    document.getElementById("p_main").src = "./projects/filmnuts/main.webp";
+    document.getElementById("p_name").innerHTML = "FILMNUTS";
+    document.getElementById("p_github").href = "";
+    document.getElementById("p_link").innerHTML = "WEBSITE";
+    document.getElementById("p_link").href = "https://www.msthebear.com/filmnuts/";
+
+    document.getElementById("p_description").innerHTML = "Professional video services website created in WordPress, highlightning key information about the company and what they do. Visitors can request a quote directly from this page to streamline their inquiry process, they can also view prices and delivery time. An important aspect for the company was also to include before/after sliders of their color corrected images. ";
+
+    document.getElementById("p_mini1").src = "./projects/filmnuts/filmnuts_th.webp";
+    document.getElementById("p_mini2").src = "./projects/filmnuts/filmnuts_1.webp";
+    document.getElementById("p_mini3").src = "./projects/filmnuts/filmnuts_2.webp";
+    document.getElementById("p_mini4").src = "./projects/filmnuts/filmnuts_3.webp";
+
+    document.getElementById("p_mini_modal1").src = "./projects/filmnuts/filmnuts_th.webp";
+    document.getElementById("p_mini_modal2").src = "./projects/filmnuts/filmnuts_1.webp";
+    document.getElementById("p_mini_modal3").src = "./projects/filmnuts/filmnuts_2.webp";
+    document.getElementById("p_mini_modal4").src = "./projects/filmnuts/filmnuts_3.webp";
+}
+
+function projectSwitch4() {
+    document.getElementById("p_main").src = "./projects/dev portfolio/main.webp";
+    document.getElementById("p_name").innerHTML = "WEB DEV PORTFOLIO";
+    document.getElementById("p_github").href = "";
+    document.getElementById("p_link").innerHTML = "WEBSITE";
+    document.getElementById("p_link").href = "";
+
+    document.getElementById("p_description").innerHTML = "Portfolio website to showcase web dev projects using a variety of technologies.";
+
+    document.getElementById("mainLink").href = "";
+
+    document.getElementById("p_mini1").src = "./projects/dev portfolio/portfolio_th.webp";
+    document.getElementById("p_mini2").src = "./projects/dev portfolio/screenshot_1.webp";
+    document.getElementById("p_mini3").src = "./projects/dev portfolio/screenshot_2.webp";
+    document.getElementById("p_mini4").src = "./projects/dev portfolio/screenshot_3.webp";
+
+    document.getElementById("p_mini_modal1").src = "./projects/dev portfolio/portfolio_th.webp";
+    document.getElementById("p_mini_modal2").src = "./projects/dev portfolio/screenshot_1.webp";
+    document.getElementById("p_mini_modal3").src = "./projects/dev portfolio/screenshot_2.webp";
+    document.getElementById("p_mini_modal4").src = "./projects/dev portfolio/screenshot_3.webp";
+}
+
+function projectSwitch5() {
+    document.getElementById("p_main").src = "./projects/etchasketch/main.webp";
+    document.getElementById("p_name").innerHTML = "ETCH A SKETCH";
+    document.getElementById("p_github").href = "https://github.com/msTheBear/Etch-A-Sketch";
+    document.getElementById("p_link").innerHTML = "WEBSITE";
+    document.getElementById("p_link").href = "https://msthebear.github.io/Etch-A-Sketch/";
+
+    document.getElementById("p_description").innerHTML = "Classic drawing toy transformed into a virtual version. Users can change the board size and pick colors.";
+
+    document.getElementById("mainLink").href = "https://msthebear.github.io/Etch-A-Sketch/";
+
+    document.getElementById("p_mini1").src = "./projects/etchasketch/etchasketch_th.webp";
+    document.getElementById("p_mini2").src = "./projects/etchasketch/screenshot_1.webp";
+    document.getElementById("p_mini3").src = "./projects/etchasketch/screenshot_2.webp";
+    document.getElementById("p_mini4").src = "./projects/etchasketch/screenshot_3.webp";
+
+    document.getElementById("p_mini_modal1").src = "./projects/etchasketch/etchasketch_th.webp";
+    document.getElementById("p_mini_modal2").src = "./projects/etchasketch/screenshot_1.webp";
+    document.getElementById("p_mini_modal3").src = "./projects/etchasketch/screenshot_2.webp";
+    document.getElementById("p_mini_modal4").src = "./projects/etchasketch/screenshot_3.webp";
+}
+
+function projectSwitch6() {
+    document.getElementById("p_main").src = "./projects/rockpaperscissors/main.webp";
+    document.getElementById("p_name").innerHTML = "ROCK PAPER SCISSORS";
+    document.getElementById("p_github").href = "https://github.com/msTheBear/Rock-Paper-Scissors";
+    document.getElementById("p_link").innerHTML = "WEBSITE";
+    document.getElementById("p_link").href = "https://msthebear.github.io/Rock-Paper-Scissors/";
+
+    document.getElementById("p_description").innerHTML = "Classic hand game usually played between two people. Visitors can now take their chances and play with a computer.";
+
+    document.getElementById("mainLink").href = "https://msthebear.github.io/Rock-Paper-Scissors/";
+
+    document.getElementById("p_mini1").src = "./projects/rockpaperscissors/rockpaperscissors_th.webp";
+    document.getElementById("p_mini2").src = "./projects/rockpaperscissors/screenshot_1.webp";
+    document.getElementById("p_mini3").src = "./projects/rockpaperscissors/screenshot_2.webp";
+    document.getElementById("p_mini4").src = "./projects/rockpaperscissors/screenshot_3.webp";
+
+    document.getElementById("p_mini_modal1").src = "./projects/rockpaperscissors/rockpaperscissors_th.webp";
+    document.getElementById("p_mini_modal2").src = "./projects/rockpaperscissors/screenshot_1.webp";
+    document.getElementById("p_mini_modal3").src = "./projects/rockpaperscissors/screenshot_2.webp";
+    document.getElementById("p_mini_modal4").src = "./projects/rockpaperscissors/screenshot_3.webp";
+}
+
+function projectSwitch7() {
+    document.getElementById("p_main").src = "./projects/michalportfolio/main.webp";
+    document.getElementById("p_name").innerHTML = "MICHAL SZYMCZAK PORTFOLIO (OLD)";
+    document.getElementById("p_github").href = "";
+    document.getElementById("p_link").innerHTML = "WEBSITE";
+    document.getElementById("p_link").href = "https://www.msthebear.com/oldwebsite/";
+
+    document.getElementById("p_description").innerHTML = "Videography portfolio created in WordPress showcasing Videographer's projects, equipment and software he uses.";
+
+    document.getElementById("mainLink").href = "https://www.msthebear.com/oldwebsite/";
+
+    document.getElementById("p_mini1").src = "./projects/michalportfolio/michalportfolio_th.webp";
+    document.getElementById("p_mini2").src = "./projects/michalportfolio/screenshot_1.webp";
+    document.getElementById("p_mini3").src = "./projects/michalportfolio/screenshot_2.webp";
+    document.getElementById("p_mini4").src = "./projects/michalportfolio/screenshot_3.webp";
+
+    document.getElementById("p_mini_modal1").src = "./projects/michalportfolio/michalportfolio_th.webp";
+    document.getElementById("p_mini_modal2").src = "./projects/michalportfolio/screenshot_1.webp";
+    document.getElementById("p_mini_modal3").src = "./projects/michalportfolio/screenshot_2.webp";
+    document.getElementById("p_mini_modal4").src = "./projects/michalportfolio/screenshot_3.webp";
+}
+
+function projectSwitch8() {
+    document.getElementById("p_main").src = "./projects/pulmonika/main.webp";
+    document.getElementById("p_name").innerHTML = "MONIKA PUL PORTFOLIO";
+    document.getElementById("p_github").href = "";
+    document.getElementById("p_link").innerHTML = "WEBSITE";
+    document.getElementById("p_link").href = "https://pulmonika.com/";
+
+    document.getElementById("p_description").innerHTML = "Editor/Colorist's personal website showcasing their work including showreels, instagram posts and credits list. Website was created using WordPress.";
+
+    document.getElementById("mainLink").href = "https://pulmonika.com/";
+
+    document.getElementById("p_mini1").src = "./projects/pulmonika/pulmonika_th.webp";
+    document.getElementById("p_mini2").src = "./projects/pulmonika/screenshot_1.webp";
+    document.getElementById("p_mini3").src = "./projects/pulmonika/screenshot_2.webp";
+    document.getElementById("p_mini4").src = "./projects/pulmonika/screenshot_3.webp";
+
+    document.getElementById("p_mini_modal1").src = "./projects/pulmonika/pulmonika_th.webp";
+    document.getElementById("p_mini_modal2").src = "./projects/pulmonika/screenshot_1.webp";
+    document.getElementById("p_mini_modal3").src = "./projects/pulmonika/screenshot_2.webp";
+    document.getElementById("p_mini_modal4").src = "./projects/pulmonika/screenshot_3.webp";
+}
+
+function projectSwitch9() {
+    document.getElementById("p_main").src = "./projects/foxit/main.webp";
+    document.getElementById("p_name").innerHTML = "FOX IT - GAME";
+    document.getElementById("p_github").href = "";
+    document.getElementById("p_link").innerHTML = "WEBSITE";
+    document.getElementById("p_link").href = "https://play.unity.com/mg/other/build-fox-it";
+
+    document.getElementById("p_description").innerHTML = 'Simple game created in Unity, based on a very popular game called "Flappy Bird".';
+
+    document.getElementById("mainLink").href = "https://play.unity.com/mg/other/build-fox-it";
+
+    document.getElementById("p_mini1").src = "./projects/foxit/foxit_th.webp";
+    document.getElementById("p_mini2").src = "./projects/foxit/screenshot_1.webp";
+    document.getElementById("p_mini3").src = "./projects/foxit/screenshot_2.webp";
+    document.getElementById("p_mini4").src = "./projects/foxit/screenshot_3.webp";
+
+    document.getElementById("p_mini_modal1").src = "./projects/foxit/foxit_th.webp";
+    document.getElementById("p_mini_modal2").src = "./projects/foxit/screenshot_1.webp";
+    document.getElementById("p_mini_modal3").src = "./projects/foxit/screenshot_2.webp";
+    document.getElementById("p_mini_modal4").src = "./projects/foxit/screenshot_3.webp";
+}
+
+
+function p1Function() {
+    projectSwitch1();
+    projectsTab();
+}
+
+function p2Function() {
+    projectSwitch2();
+    projectsTab();
+}
+
+function p3Function() {
+    projectSwitch3();
+    projectsTab();
+}
+
+function p4Function() {
+    projectSwitch4();
+    projectsTab();
+}
+
+function p5Function() {
+    projectSwitch5();
+    projectsTab();
+}
+
+function p6Function() {
+    projectSwitch6();
+    projectsTab();
+}
+
+function p7Function() {
+    projectSwitch7();
+    projectsTab();
+}
+
+function p8Function() {
+    projectSwitch8();
+    projectsTab();
+}
+
+function p9Function() {
+    projectSwitch9();
+    projectsTab();
+}
+
+$("#button1, #button2, #button3, #button4, #button5, #button6, #button7, #button8, #button9").click(function () {
+    $("#p_main").fadeOut(0, function () {
+        $("#p_main").attr("src", $("#link").attr("href"));
+    }).fadeIn(400);
+    return false;
+});
+
+$("#button1, #button2, #button3, #button4, #button5, #button6, #button7, #button8, #button9").click(function () {
+    $("#p_mini1").fadeOut(0, function () {
+        $("#p_mini1").attr("src", $("#link").attr("href"));
+    }).fadeIn(400);
+    return false;
+});
+
+$("#button1, #button2, #button3, #button4, #button5, #button6, #button7, #button8, #button9").click(function () {
+    $("#p_mini2").fadeOut(0, function () {
+        $("#p_mini2").attr("src", $("#link").attr("href"));
+    }).fadeIn(400);
+    return false;
+});
+
+$("#button1, #button2, #button3, #button4, #button5, #button6, #button7, #button8, #button9").click(function () {
+    $("#p_mini3").fadeOut(0, function () {
+        $("#p_mini3").attr("src", $("#link").attr("href"));
+    }).fadeIn(400);
+    return false;
+});
+
+$("#button1, #button2, #button3, #button4, #button5, #button6, #button7, #button8, #button9").click(function () {
+    $("#p_mini4").fadeOut(0, function () {
+        $("#p_mini4").attr("src", $("#link").attr("href"));
+    }).fadeIn(400);
+    return false;
+});
+
+$("#button1, #button2, #button3, #button4, #button5, #button6, #button7, #button8, #button9").click(function () {
+    $("#p_description").fadeOut(0, function () {
+        $("#p_description").attr("src", $("#link").attr("href"));
+    }).fadeIn(400);
+    return false;
+});
+
+$("#button1, #button2, #button3, #button4, #button5, #button6, #button7, #button8, #button9").click(function () {
+    $("#p_name").fadeOut(0, function () {
+        $("#p_name").attr("src", $("#link").attr("href"));
+    }).fadeIn(400);
+    return false;
+});
+
+$("#button1, #button2, #button3, #button4, #button5, #button6, #button7, #button8, #button9").click(function () {
+    $("#p_github").fadeOut(0, function () {
+        $("#p_github").attr("src", $("#link").attr("href"));
+    }).fadeIn(400);
+    return false;
+});
+
+$("#button1, #button2, #button3, #button4, #button5, #button6, #button7, #button8, #button9").click(function () {
+    $("#p_link").fadeOut(0, function () {
+        $("#p_link").attr("src", $("#link").attr("href"));
+    }).fadeIn(400);
+    return false;
+});
+
